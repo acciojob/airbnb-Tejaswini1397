bd2f3f9ff2b9a806b0da341d6c59f07275f6a8ab
Tejaswini Khairnar <tejaswinikhairnar97@gmail.com>
2023-07-12 16:52:27 +0530

    add

--- src/main/java/com/driver/controllers/HotelManagementRepository.java
+++ src/main/java/com/driver/controllers/HotelManagementRepository.java
@@ -73,37 +73,39 @@ public class HotelManagementRepository {
         //Calculate the total amount paid by the person based on no. of rooms booked and price of the room per night.
         //If there arent enough rooms available in the hotel that we are trying to book return -1
         //in other case return total amount paid
-//        String bookingId = UUID.randomUUID().toString();
-//        booking.setBookingId(bookingId);
+        String bookingId = UUID.randomUUID().toString();
+        booking.setBookingId(bookingId);
 //
 //        // Calculate the total amount paid by the person based on the number of rooms booked and price of the room per night
-//        int totalAmountPaid = booking.getNoOfRooms() * booking.getAmountToBePaid();
-//        bookingMap.put(bookingId,booking);
-//
+      //  int totalAmountPaid = booking.getNoOfRooms() * booking.getAmountToBePaid();
+      //  bookingMap.put(bookingId,booking);
+
 //        // Check if there are enough rooms available in the hotel
-//        Hotel hotel = hotelDb.get(booking.getHotelName());
-//        if (hotel != null && hotel.getAvailableRooms() >= booking.getNoOfRooms()) {
-//            // Update the number of available rooms in the hotel
-//            hotel.setAvailableRooms(hotel.getAvailableRooms() - booking.getNoOfRooms());
-//            return totalAmountPaid;
-//        } else {
-//            return -1; // Not enough rooms available
-//        }
-        String hotelName=booking.getHotelName();
-        if(!hotelDb.containsKey(hotelName))return -1;
-        if(hotelDb.get(hotelName).getAvailableRooms()>=booking.getNoOfRooms()){
-            Hotel hotel=hotelDb.get(hotelName);
-            int totalAvailable=hotel.getAvailableRooms();
-            totalAvailable-=booking.getNoOfRooms();
-            hotel.setAvailableRooms(totalAvailable);
-            hotelDb.put(hotelName,hotel);
-            String bookinId= String.valueOf(UUID.randomUUID());
-            int amountTobePaid=hotel.getPricePerNight()*booking.getNoOfRooms();
-            bookingMap.put(bookinId,booking);
-            userRent.put(bookinId,amountTobePaid);
+        Hotel hotel = hotelDb.get(booking.getHotelName());
+        if (hotel != null && hotel.getAvailableRooms() >= booking.getNoOfRooms()) {
+            // Update the number of available rooms in the hotel
+            hotel.setAvailableRooms(hotel.getAvailableRooms() - booking.getNoOfRooms());
+           int amountTobePaid=hotel.getPricePerNight()*booking.getNoOfRooms();
+            bookingMap.put(bookingId,booking);
             return amountTobePaid;
+        } else {
+            return -1; // Not enough rooms available
         }
-        return -1;
+//        String hotelName=booking.getHotelName();
+//        if(!hotelDb.containsKey(hotelName))return -1;
+//        if(hotelDb.get(hotelName).getAvailableRooms()>=booking.getNoOfRooms()){
+//            Hotel hotel=hotelDb.get(hotelName);
+//            int totalAvailable=hotel.getAvailableRooms();
+//            totalAvailable-=booking.getNoOfRooms();
+//            hotel.setAvailableRooms(totalAvailable);
+//            hotelDb.put(hotelName,hotel);
+//            String bookinId= String.valueOf(UUID.randomUUID());
+//            int amountTobePaid=hotel.getPricePerNight()*booking.getNoOfRooms();
+//            bookingMap.put(bookinId,booking);
+//            userRent.put(bookinId,amountTobePaid);
+//            return amountTobePaid;
+//        }
+//        return -1;
 
     }
     public int getBookings(Integer aadharCard) {
